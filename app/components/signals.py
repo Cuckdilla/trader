b67e7689f47ad3509febc99cb87d5860793b230c
winterbro <joakim.winther@gmail.com>
2021-07-31 00:09:14 +0200

    Added more indicators and changed weight of bearish signals to negative values

--- app/components/signals.py
+++ app/components/signals.py
@@ -75,14 +75,14 @@ class Signals:
             # Below threshold of 20, indicates RSI oversold
             signal_name = "stochastic_oversold"
             if k < 20 and d < 20:
-                self.add_signal([signal_name, "Buy", "Oscillator", "K and D lines are below lower threshold (20)", 1, k])
+                self.add_signal([signal_name, "Buy", "Oscillator", "Stochastic Oversold: K and D lines are below the lower threshold (20)", 2, k])
             else:
                 self.drop_signal(signal_name)
 
             # Above threshold of 80, indicates RSI oversold
             signal_name = "stochastic_overbought"
             if k > 80 and d > 80:
-                self.add_signal([signal_name, "Sell", "Oscillator", "K and D lines are above upper threshold (80)", 1, k])
+                self.add_signal([signal_name, "Sell", "Oscillator", "Stochastic Overbought: K and D lines are above the upper threshold (80)", -2, k])
             else:
                 self.drop_signal(signal_name)
 
@@ -90,7 +90,7 @@ class Signals:
             # K line higher than D line indicates a bullish market
             signal_name = "stochastic_bullish_market"
             if k > d:
-                self.add_signal([signal_name, "Neutral", "Oscillator", "K line is above D, indicating bullish market", 1, k])
+                self.add_signal([signal_name, "Buy", "Oscillator", "Stochastic Bullish Market: K line is above D", 1, k])
 
                 self.market_is_bullish = True
             else:
@@ -100,7 +100,7 @@ class Signals:
             # Bearish market
             signal_name = "stochastic_bearish_market"
             if k < d:
-                self.add_signal([signal_name, "Neutral", "Oscillator", "K line is below D, indicating bearish market", 1, k])                
+                self.add_signal([signal_name, "Sell", "Oscillator", "Stochastic Bearish Market: K line is below D", -1, k])                
 
                 self.market_is_bullish = False
             else:
@@ -109,15 +109,15 @@ class Signals:
             # Bullish crossover 
             signal_name = "stochastic_bullish_crossover"
             if k > d and previous_k < previous_d:
-                self.add_signal([signal_name, "Buy", "Oscillator", "K crossed over D", 1, k])
+                self.add_signal([signal_name, "Buy", "Oscillator", "Stochastic Bullish Crossover: K crossed over D", 2, k])
 
             else:
                 self.drop_signal(signal_name)
 
             # Bearish crossover 
-            signal_name = "stochastic_bullish_crossover"
+            signal_name = "stochastic_bearish_crossover"
             if k < d and previous_k > previous_d:
-                self.add_signal([signal_name, "Sell", "Oscillator", "D crossed over K", 1, k])
+                self.add_signal([signal_name, "Sell", "Oscillator", "Stochastic Bearish Crossover: D crossed over K", -2, k])
 
             else:
                 self.drop_signal(signal_name)
@@ -137,32 +137,54 @@ class Signals:
         moving_averages = ["SMA-200-close", "SMA-100-close", "SMA-50-close", "SMA-20-close"]
         last_close = chart["close"].iat[-1]
         previous_close = chart["close"].iat[-2]
+        last_open = chart["open"].iat[-1]
 
         for ma in moving_averages:
                 
             if ma in chart:
                 
-                # Price above average
-                signal_name = f"price_above_{ma}"
+                # Bullish market
+                signal_name = f"{ma}_bullish_market"
 
                 if last_close > chart[ma].iat[-1]:
                     self.log.debug(f"Price is higher than {ma}")
                     
-                    self.add_signal([signal_name, "Neutral", "Average", f"Closing higher than Moving Average ({ma})", 1, chart[ma].iat[-1]])
+                    self.add_signal([signal_name, "Buy", "Average", f"Moving Average {ma} bullish market: Closing price is above {ma}", 1, chart[ma].iat[-1]])
 
                 else:
                     self.log.debug(f"Price is lower than {ma}")
                     self.drop_signal(signal_name)
 
-                # Crossover
-                signal_name = f"price_crossed_over_{ma}"
+                # Bearish market
+                signal_name = f"{ma}_bearish_market"
+
+                if last_close < chart[ma].iat[-1]:
+                    self.log.debug(f"Price is lower than {ma}")
+                    
+                    self.add_signal([signal_name, "Sell", "Average", f"Moving Average {ma} bearish market: Closing price is below {ma}", -1, chart[ma].iat[-1]])
+
+                else:
+                    self.log.debug(f"Price is higher than {ma}")
+                    self.drop_signal(signal_name)
+
+                # Bullish crossover
+                signal_name = f"price_crossed_over_{ma}" 
                 if last_close > chart[ma].iat[-1] and previous_close < chart[ma].iat[-2]:
                     self.log.debug(f"Price crossed over {ma}")
-                    self.add_signal([signal_name, "Neutral", "Average", f"Price crossed over {ma}", 1, chart[ma].iat[-1]])
+                    self.add_signal([signal_name, "Buy", "Average", f"Moving Average {ma} bullish crossover: Closing price crossed {ma}", 2, chart[ma].iat[-1]])
                 else:
-                    self.log.debug(f"Price is lower than {ma}")
+                    self.log.debug(f"Price is higher than {ma}")
                     self.drop_signal(signal_name)
 
+
+                # Bearish crossover
+                signal_name = f"price_crossed_over_{ma}" 
+                if last_close < chart[ma].iat[-1] and previous_close > chart[ma].iat[-2]:
+                    self.log.debug(f"Price crossed over {ma}")
+                    self.add_signal([signal_name, "Sell", "Average", f"Moving Average {ma} bearish crossover: Closing price crossed {ma}", -2, chart[ma].iat[-1]])
+                else:
+                    self.log.debug(f"Price is higher than {ma}")
+                    self.drop_signal(signal_name)
 
 #
 # RELATIVE STRENGTH INDEX
@@ -177,12 +199,24 @@ class Signals:
 
         self.log.debug("Evaluating indicator: Trading volume")
 
+        # Volume SMA bullish market
         signal_name = f"volume_above_{sma}"
 
         if sma in chart:
             self.log.debug("Volume: {} ({}: {})".format(chart["volume"].iat[-1], sma, chart["SMA-20-volume"].iat[-1]))
-            if chart["volume"].iat[-1] > chart[sma].iat[-1]:
-                self.add_signal([signal_name, "Neutral", "Average", f"Trading volume is higher than {sma}", 1, chart["volume"].iat[-1]])
+            if chart["volume"].iat[-1] > chart[sma].iat[-1] and last_close > last_open:
+                self.add_signal([signal_name, "Buy", "Average", f"Trading volume is higher than {sma}", 2, chart["volume"].iat[-1]])
+
+        else:
+            self.log.warning(f"{sma} has not yet been calculated.")
+
+        # Volume SMA bearish market
+        signal_name = f"volume_below_{sma}"
+
+        if sma in chart:
+            self.log.debug("Volume: {} ({}: {})".format(chart["volume"].iat[-1], sma, chart["SMA-20-volume"].iat[-1]))
+            if chart["volume"].iat[-1] > chart[sma].iat[-1] and last_close < last_open:
+                self.add_signal([signal_name, "Sell", "Average", f"Trading volume is higher than {sma}", -2, chart["volume"].iat[-1]])
 
         else:
             self.log.warning(f"{sma} has not yet been calculated.")
@@ -213,7 +247,7 @@ class Signals:
             signal_name = "macd_bullish_crossover"
             if macd > signal and previous_macd < previous_signal:
                 self.log.debug("MACD crossed over the signal line")
-                self.add_signal([signal_name, "Buy", "Oscillator", "MACD crossed over the signal line", 1, macd]) 
+                self.add_signal([signal_name, "Buy", "Oscillator", "MACD Bullish Market: MACD is above the signal line", 1, macd]) 
             else:
                 self.drop_signal(signal_name)
 
@@ -221,7 +255,7 @@ class Signals:
             signal_name = "macd_bearish_crossover"
             if macd < signal and previous_macd > previous_signal:
                 self.log.debug("Signal line crossed over MACD")
-                self.add_signal([signal_name, "Sell", "Oscillator", "The signal line crossed over the MACD", 1, macd]) 
+                self.add_signal([signal_name, "Sell", "Oscillator", "MACD Bearish Market: The signal line is above MACD", -1, macd]) 
             else:
                 self.drop_signal(signal_name)
 
